db7eb0516bdcc904582a56194e1c45d693ab0eeb
Vladislav <vlad.kvan96@gmail.com>
2026-09-19 12:24:46 +0300

    feat: add download section

--- src/js/sections/download.js
+++ src/js/sections/download.js
@@ -1,5 +1,33 @@
 import { createMyElement } from './../common/functions';
 
 const download = createMyElement('section', '', 'download');
+
+download.innerHTML = `
+<div class="download-info">
+  <h2>
+    <span class="highlight">Download</span> our app<br /> to start ordering
+  </h2>
+
+  <p class="download-desc">
+    Download the Resource app today and experience the comfort of ordering
+    your favorite coffee from wherever you are
+  </p>
+  <div class="download-btn">
+    <a href="#">
+      <span class="btn-icon apple"></span>
+      <p class="btn-title">Available on the</p>
+      <p class="btn-subtitle">App Store</p>
+    </a>
+    <a href="#">
+      <span class="btn-icon google"></span>
+      <p class="btn-title">Available on</p>
+      <p class="btn-subtitle">Google Play</p>
+    </a>
+  </div>
+</div>
+<div class="download-phones">
+  <img src="./src/assets/mobile-screens.png" alt="Coffee app on smartphone">
+</div>
+`;
   
 export default download;
